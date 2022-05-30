76d40763cba85da3d1204c88d6e677de4d57a527
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-05-30 16:19:25 +0200

    Add DB support

---
+++ hs-api/src/hs_api/db.clj
@@ -0,0 +1,27 @@
+(ns hs-api.db
+  (:require [hugsql.core :as hugsql])
+  (:require [clojure.java.jdbc :as j]))
+
+
+(def db {:dbtype "postgresql"
+            :dbname "hs-db"
+            :host (or (System/getenv "DB_HOST") "localhost")
+            :user (or (System/getenv "DB_USER") "postgres")
+            :password (System/getenv "DB_PASSWORD")})
+
+(hugsql/def-db-fns "hs_api/queries/patients.sql")
+
+;; Create table patients if not exists
+(create-parients-table db)
+
+;; Create a signle patient
+(defn create-patient [name]
+  (insert-patient db {:name name}))
+
+;; Get a single patient by id
+(defn get-patient-by-id [id]
+  (patient-by-id {:id id}))
+
+;; (all-patients db)
+;; (get-patient-by-id "b1393a03-8453-4f65-8b58-fd5631e66d66")
+;; (create-patient "Donald Trump")
---
+++ hs-api/src/hs_api/queries/patients.sql
@@ -0,0 +1,30 @@
+-- :name create-parients-table
+-- :command :execute
+-- :result :raw
+-- :doc Create patients table
+CREATE TABLE IF NOT EXISTS patients
+(
+id uuid NOT NULL DEFAULT gen_random_uuid(),
+name character varying NOT NULL,
+address character varying,
+oms numeric,
+PRIMARY KEY (id)
+)
+
+
+-- :name all-patients
+-- :doc Get all patients
+SELECT *
+FROM patients
+
+-- :name patient-by-id
+-- :doc Get a single patient by id
+SELECT *
+FROM patients
+WHERE id::text = :id
+
+-- :name insert-patient :<!
+-- :doc Insert a single patient
+insert into patients (name)
+values (:name)
+returning id
