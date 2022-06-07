7a0603aea50cd9d36b3b400840265d1255ca383a
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-06-07 23:38:53 +0200

    Add skeleton css
    Signed-off-by: Kirill Stepanov <kirill.stepanov@cosuno.de>

--- src/cljs/hs_api/core.cljs
+++ src/cljs/hs_api/core.cljs
@@ -5,22 +5,34 @@
    [helix.dom :as d]
    ["react-dom" :as rdom]))
 
-
 ;; define components using the `defnc` macro
 (defnc greeting
   "A component which greets a user."
   [{:keys [name]}]
   ;; use helix.dom to create DOM elements
-  (d/div "Hello, " (d/strong name) "!"))
+  (d/label "Hello, " (d/strong name) "!"))
 
 (defnc app []
   (let [[state set-state] (hooks/use-state {:name "Helix User"})]
     (d/div
-     (d/h1 "Welcome!")
+     {:class-name "container"}
+     (d/div
+      {:class-name "row"}
+      (d/h1 "Welcome!"))
      ;; create elements out of components
-     ($ greeting {:name (:name state)})
-     (d/input {:value (:name state)
-               :on-change #(set-state assoc :name (.. % -target -value))}))))
+     (d/div
+      {:class-name "row"}
+      ($ greeting {:name (:name state)}))
+     (d/div
+      {:class-name "row"}
+      (d/input {:class-name "u-full-width"
+                :type "text"
+                :value (:name state)
+                :on-change #(set-state assoc :name (.. % -target -value))}))
+     (d/div
+      {:class-name "row"}
+      (d/button {:class-name "button-primary"
+                 :type "button"} "Submit something")))))
 
 (defn render []
   (rdom/render ($ app) (js/document.getElementById "app")))
