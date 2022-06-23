1c05cb6572a789775f130dfb39cc01e81afade59
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-06-24 00:33:12 +0200

    Add update endpoint, improve naming

--- src/cljs/hs_api/client.cljs
+++ src/cljs/hs_api/client.cljs
@@ -1,11 +1,18 @@
 (ns hs-api.client
   (:require [ajax.core :refer [GET POST]]))
 
-(defn get-patients [handler error-handler]
-  (GET "/api/patients" {:handler handler
+(defn search-patients [query handler error-handler]
+  (GET "/api/patients" {:url-params {:query query}
+                        :handler handler
                         :error-handler error-handler
                         :response-format :json
                         :keywords? true}))
+
+(defn get-patient-by-id [id handler error-handler]
+  (GET (str "/api/patients/" id) {:handler handler
+                                 :error-handler error-handler
+                                 :response-format :json
+                                 :keywords? true}))
 
 (defn create-patient [patient handler error-handler]
   (POST "/api/patients" {:params patient
@@ -14,3 +21,11 @@
                          :format :json
                          :response-format :json
                          :keywords? true}))
+
+(defn update-patient [id patient handler error-handler]
+  (PUT (str "/api/patients/" id) {:params patient
+                                  :handler handler
+                                  :error-handler error-handler
+                                  :format :json
+                                  :response-format :json
+                                  :keywords? true}))
