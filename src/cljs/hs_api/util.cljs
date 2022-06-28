f4c965145c780b2c30f4628ce1342b3de211fdf9
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-06-28 16:29:03 +0200

    Implement use-request hook

---
+++ src/cljs/hs_api/util.cljs
@@ -0,0 +1,18 @@
+(ns hs-api.util
+  (:require [helix.hooks :as hooks]))
+
+(defn use-request [request-fn]
+  (let [[in-progress set-in-progress] (hooks/use-state false)
+        [error set-error] (hooks/use-state nil)
+        [result set-result] (hooks/use-state nil)
+        handle-success #(do (set-in-progress false)
+                            (set-result %))
+        handle-error #(do (set-in-progress false)
+                          (set-error %))]
+    [(fn [args]
+       (do
+         (set-in-progress true)
+         (apply request-fn (conj [] args handle-success handle-error))))
+     in-progress
+     result
+     error]))
