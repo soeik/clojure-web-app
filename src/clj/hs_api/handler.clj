119f6c5937c58be410708f7573c65a34996cc786
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-06-29 00:28:08 +0200

    Do not use ring.util.http-response for PUT and POST

--- src/clj/hs_api/handler.clj
+++ src/clj/hs_api/handler.clj
@@ -3,8 +3,11 @@
             [compojure.route :as route]
             [ring.middleware.data.json :refer [wrap-json-request
                                                wrap-json-response]]
-            [ring.middleware.defaults :refer :all]
-            [ring.util.http-response :refer :all]
+            [ring.middleware.defaults :refer [wrap-defaults
+                                              site-defaults]]
+            [ring.util.http-response :refer [ok
+                                             bad-request
+                                             not-found]]
             [hs-api.patient :refer [patient-valid?]]
             [hs-api.db :as db]))
 
@@ -19,8 +22,10 @@
                                      (db/search-patients query)
                                      (db/get-all-patients))))))
            (POST "/" {body :body} (if (patient-valid? body)
-                                      (created (db/create-patient body))
-                                      (bad-request "Invalid input")))
+                                    {:status 201
+                                     :body (map id->str (db/create-patient body))}
+                                    {:status 400
+                                     :body "Invalid input"}))
            (context "/:id" [id]
                     (GET "/" []
                          (let [patient (db/get-patient id)]
@@ -28,38 +33,32 @@
                              (ok patient)
                              (not-found "Patient not found"))))
                     (PUT "/" {body :body} (if (patient-valid? body)
-                                              (ok (db/update-patient id body))
-                                              (bad-request "Invalid input")))
+                                            {:status 200
+                                             :body (map id->str (db/update-patient id body))}
+                                            {:status 400
+                                             :body "Invalid input"}))
                     (DELETE "/" [] (ok (db/delete-patient id)))))
   (route/resources "/")
   (route/not-found "Not Found"))
 
-;; TODO [prone.middleware :refer [wrap-exceptions]]
 ;; TODO dev [ring.middleware.reload :refer [wrap-reload]]
 ;; TODO Filtering
 ;; TODO Improve input validation
 ;; TODO Improve error handling
 ;; TODO Tests
 ;; FIXME: Error with the date field
-(defn wrap-exception [handler]
-  (fn [request]
-    (try (handler request)
-         (catch Exception e
-           (println (str "Caught exception: " (.getMessage e)))
-           {:status 500
-            :body {:message (str "Error occured: " (.getMessage e))}}))))
 
 (def app
   (->
    app-routes
    (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
    (wrap-json-request :key-fn keyword)
-   wrap-json-response
-   wrap-exception))
+   wrap-json-response))
 
 
 (comment
   (uuid->str {:id 123})
+  (created {:foo "bar"})
   (app {:request-method :get, :uri "/api/patients"})
   (app {:request-method :get, :uri "/api/patients", :params {:query "frank"}})
   (app {:request-method :post, :uri "/api/patients", :body {:oms "1234567890123456",
