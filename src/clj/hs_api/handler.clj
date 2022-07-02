ababc36b5255836c3e72c6e269f9edd37c7dbadf
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-07-02 13:50:41 +0200

    Inject db in a request handler

--- src/clj/hs_api/handler.clj
+++ src/clj/hs_api/handler.clj
@@ -4,7 +4,7 @@
             [ring.middleware.data.json :refer [wrap-json-request wrap-json-response]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [hs-api.patient :refer [patient-valid?]]
-            [hs-api.db :as db]))
+            [hs-api.db :refer [db-client]]))
 
 (defn entry->dto [col]
   (as-> col v
@@ -13,29 +13,29 @@
 
 (defroutes app-routes
   (context "/api/patients" []
-           (GET "/" {params :query-params}
-                (let [query (get params "query")
-                      gender (get params "gender")
-                      date-of-birth (get params "date-of-birth")
-                      results (db/search-patients query gender date-of-birth)]
+           (GET "/" {:keys [db query-params]}
+                (let [ query (get query-params "query")
+                      gender (get query-params "gender")
+                      date-of-birth (get query-params "date-of-birth")
+                      results ((db :search-patients) query gender date-of-birth)]
                   {:status 200 :body (map entry->dto results)}))
-           (POST "/" {body :body} (if (patient-valid? body)
-                                    {:status 201 :body (entry->dto (db/create-patient body))}
+           (POST "/" {:keys [db body]} (if (patient-valid? body)
+                                    {:status 201 :body (entry->dto ((db :create-patient) body))}
                                     {:status 400 :body "Invalid input"}))
            (context "/:id" [id]
-                    (GET "/" []
-                         (let [patient (db/get-patient id)]
+                    (GET "/" {:keys [db]}
+                         (let [patient ((db :get-patient) id)]
                            (if (some? patient)
                              {:status 200 :body (entry->dto patient)}
                              {:status 404 :body "Patient not found"})))
-                    (PUT "/" {body :body} (if (patient-valid? body)
-                                            (let [affected-rows (db/update-patient id body)]
+                    (PUT "/" {:keys [db body]} (if (patient-valid? body)
+                                            (let [affected-rows ((db :update-patient) id body)]
                                               (if (= affected-rows 1)
                                                 {:status 200 :body {:id id}}
                                                 {:status 400 :body "Failed to update patient"}))
                                             {:status 400 :body "Invalid input"}))
-                    (DELETE "/" []
-                            (let [affected-rows (db/delete-patient id)]
+                    (DELETE "/" {:keys [db]}
+                            (let [affected-rows ((db :delete-patient) id)]
                               ;; TODO If affected more than 1 row?
                               (if (= affected-rows 1)
                                 {:status 200 :body {:id id :deleted true}}
@@ -43,35 +43,36 @@
   (route/resources "/")
   (route/not-found "Not Found"))
 
+(defn wrap-db [f db]
+  (fn [req]
+    (f (assoc req :db db))))
+
 ;; TODO Search by name case insensitive
 ;; TODO Tests
 ;; TODO Date in this century
 ;; TODO Serve index file properly
 
-(def app
+(defn create-app [db]
   (->
    app-routes
+   (wrap-db db)
    (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
    (wrap-json-request :key-fn keyword)
    wrap-json-response))
 
+(def app (create-app db-client))
 
 (comment
+  (def patient-id "1cc3719a-6470-4a81-84f6-d6c340add5036")
   (entry->dto {:id 123})
-  (app {:request-method :get, :uri "/api/patients"})
+  ((create-app db-client) {:request-method :get, :uri "/api/patients"})
   (app {:request-method :get, :uri "/api/patients", :params {:query "frank"}})
   (app {:request-method :post, :uri "/api/patients", :body {:oms "1234567890123456",
                                                             :name "Frank Cowperwood",
                                                             :gender "M"
                                                             :address "Sundgauerstr. 123" }})
-  (update-patient "86f6e38b-fa0a-40b8-bce5-903df0fb97d4"
-                  {:id "b1393a03-8453-4f65-8b58-fd5631e66d66",
-                   :name "Donald Trumpet",
-                   :address "Philadelphia ave."
-                   :gender "M"
-                   :oms "5646576767" })
   (app {:request-method :put,
-        :uri "/api/patients/86f6e38b-fa0a-40b8-bce5-903df0fb97d4",
+        :uri "/api/patients/1cc3719a-6470-4a81-84f6-d6340add5036"
         :body {:name "Donald Trumpet",
                :address "Philadelphia ave."
                :gender "M"
