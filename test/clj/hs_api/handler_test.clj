48e5d6c78b5a0c6fef46a417bfde4fdbb9539420
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-07-02 17:26:40 +0200

    Improve tests

--- test/clj/hs_api/handler_test.clj
+++ test/clj/hs_api/handler_test.clj
@@ -1,14 +1,86 @@
 (ns hs-api.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [hs-api.handler :refer :all]))
+            [hs-api.handler :refer [create-app]]))
+
+(def mock-patient {:id "u-1"
+                   :name "Test User"
+                   :gender "M"
+                   :date-of-birth "1980-01-01"
+                   :address "Berlin, Germany"
+                   :oms "1234567890123456"})
+
+(def mock-db-client {:search-patients (fn [search-query
+                                           gender
+                                           date-of-birth]
+                                        [mock-patient])
+                     :get-patient (fn [id] (if (= id "u-1") mock-patient nil))
+                     :delete-patient (fn [id] (if (= id "u-1") 1 0))
+                     :update-patient (fn [id body] (if (= id "u-1") {:id id} 0))
+                     :create-patient (fn [patient] {:id "u-1"})})
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+  (let [app (create-app mock-db-client)]
+    (testing "not-found route"
+      (let [response (app (mock/request :get "/invalid"))]
+        (is (= (:status response) 404))))
 
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+    (testing "GET /patients"
+      (let [response (app (mock/request :get "/api/patients"))]
+        (is (= (:status response) 200))
+        (is (= (count (:body response)) 1))
+        (is (= (:body response) [mock-patient]))))
+
+    (testing "GET /patients/:id"
+      (testing "returns a patient"
+        (let [response (app (mock/request :get "/api/patients/u-1"))]
+          (is (= (:status response) 200))
+          (is (= (:body response) mock-patient))))
+
+      (testing "returns status 404 if patient doesn't exist"
+        (let [response (app (mock/request :get "/api/patients/u-2"))]
+          (is (= (:status response) 404)))))
+
+    (testing "DELETE /patients/:id"
+      (testing "returns ok if patient deleted"
+        (let [response (app (mock/request :delete "/api/patients/u-1"))]
+          (is (= (:status response) 200))))
+
+      (testing "returns status 400 if deletion failed"
+        (let [response (app (mock/request :delete "/api/patients/u-2"))]
+          (is (= (:status response) 400)))))
+
+    (testing "POST /patients"
+      (testing "returns status 400 when input is invalid"
+        (let [response (app {:request-method :post
+                             :uri "/api/patients/"
+                             :body {:foo "bar"}})]
+          (is (= (:status response) 400))))
+
+      (testing "returns an id if request succeeded"
+        (let [response (app {:request-method :post,
+                             :uri "/api/patients/"
+                             :body (dissoc mock-patient :id)})]
+          (is (= (:status response) 201))
+          (is (= (:id (:body response)) "u-1")))))
+
+    (testing "PUT /patients/:id"
+      (testing "updates patient"
+        (let [response (app {:request-method :put
+                             :uri "/api/patients/u-1"
+                             :body (dissoc mock-patient :id)})]
+          (is (= (:status response) 200))))
+
+      (testing "returns  Invalid input if patient is invalid"
+        (let [response (app {:request-method :put
+                             :uri "/api/patients/u-1"
+                             :body {}})]
+          (is (= (:status response) 400))
+          (is (= (:body response) "Invalid input"))))
+
+      (testing "returns Failed to update patient if patient not found"
+        (let [response (app {:request-method :put
+                             :uri "/api/patients/u-2"
+                             :body mock-patient})]
+          (is (= (:status response) 400))
+          (is (= (:body response) "Failed to update patient")))))))
--- test/cljc/hs_api/validation_test.clj
+++ test/cljc/hs_api/validation_test.clj
@@ -9,14 +9,14 @@
     (is (= (oms-valid? "1234567890123456") true))))
 
 (deftest test-date-of-birth-valid?
-  (testing "validates date input")
-  (is (= (date-of-birth-valid? "") false))
-  (is (= (date-of-birth-valid? "12345") false))
-  (is (= (date-of-birth-valid? nil) false))
-  (is (= (date-of-birth-valid? "2000-50-20") false))
-  (is (= (date-of-birth-valid? "1970-01-01") true)))
+  (testing "validates date input"
+    (is (= (date-of-birth-valid? "") false))
+    (is (= (date-of-birth-valid? "12345") false))
+    (is (= (date-of-birth-valid? nil) false))
+    (is (= (date-of-birth-valid? "2000-50-20") false))
+    (is (= (date-of-birth-valid? "1970-01-01") true))))
 
 (deftest test-date-of-birth-in-past?
-  (testing "checks if the date is in the past")
-  (is (= (date-of-birth-in-past? "3000-05-05") false))
-  (is (= (date-of-birth-in-past? "1970-01-01") true)))
+  (testing "checks if the date is in the past"
+    (is (= (date-of-birth-in-past? "3000-05-05") false))
+    (is (= (date-of-birth-in-past? "1970-01-01") true))))
