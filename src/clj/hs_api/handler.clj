7b97d87182a263553217265a8cdc1b85d921a8dc
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-07-02 17:25:57 +0200

    Serve index.html for app routes

--- src/clj/hs_api/handler.clj
+++ src/clj/hs_api/handler.clj
@@ -1,17 +1,20 @@
 (ns hs-api.handler
-  (:require [compojure.core :refer :all]
+  (:require [clojure.java.io :as io]
+            [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.data.json :refer [wrap-json-request wrap-json-response]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [hs-api.patient :refer [patient-valid?]]
             [hs-api.db :refer [db-client]]))
 
+(defn index-handler [req] (io/file "resources/public/index.html"))
+
 (defn entry->dto [col]
   (as-> col v
       (update v :date-of-birth str)
       (update v :id str)))
 
-(defroutes app-routes
+(defroutes api-routes
   (context "/api/patients" []
            (GET "/" {:keys [db query-params]}
                 (let [ query (get query-params "query")
@@ -20,8 +23,9 @@
                       results ((db :search-patients) query gender date-of-birth)]
                   {:status 200 :body (map entry->dto results)}))
            (POST "/" {:keys [db body]} (if (patient-valid? body)
-                                    {:status 201 :body (entry->dto ((db :create-patient) body))}
-                                    {:status 400 :body "Invalid input"}))
+                                         ;; TODO entry-dto adds date-of-birth
+                                         {:status 201 :body (entry->dto ((db :create-patient) body))}
+                                         {:status 400 :body "Invalid input"}))
            (context "/:id" [id]
                     (GET "/" {:keys [db]}
                          (let [patient ((db :get-patient) id)]
@@ -36,10 +40,14 @@
                                             {:status 400 :body "Invalid input"}))
                     (DELETE "/" {:keys [db]}
                             (let [affected-rows ((db :delete-patient) id)]
-                              ;; TODO If affected more than 1 row?
                               (if (= affected-rows 1)
                                 {:status 200 :body {:id id :deleted true}}
-                                {:status 400 :body "Failed to delete patient"})))))
+                                {:status 400 :body "Failed to delete patient"}))))))
+
+(defroutes site-routes
+  (GET "/" [] index-handler)
+  (GET "/patients" [] index-handler)
+  (GET "/patients/:id" [] index-handler)
   (route/resources "/")
   (route/not-found "Not Found"))
 
@@ -50,17 +58,14 @@
 ;; TODO Search by name case insensitive
 ;; TODO Tests
 ;; TODO Date in this century
-;; TODO Serve index file properly
 
-(defn create-app [db]
-  (->
-   app-routes
-   (wrap-db db)
-   (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
-   (wrap-json-request :key-fn keyword)
-   wrap-json-response))
+(defn create-app [db] (wrap-db (routes api-routes site-routes) db))
 
-(def app (create-app db-client))
+(def app (->
+          (create-app db-client)
+          (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
+          (wrap-json-request :key-fn keyword)
+          wrap-json-response))
 
 (comment
   (def patient-id "1cc3719a-6470-4a81-84f6-d6c340add5036")
@@ -77,6 +82,4 @@
                :address "Philadelphia ave."
                :gender "M"
                :oms "1234567890123456"}})
-  (app {:request-method :get, :uri "/api/patients/non-existings"})
-  (app {:request-method :get, :uri "/api/patients/b1393a03-8453-4f65-8b58-fd5631e66d66"})
   (app {:request-method :delete, :uri "/api/patients/57c181d4-63f0-423a-8f57-96528b8a0695"}))
