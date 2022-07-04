ca09c6b549200d55d63cb841949fc903cc68b17d
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-07-04 22:52:22 +0200

    Fix require

--- src/clj/hs_api/db.clj
+++ src/clj/hs_api/db.clj
@@ -1,6 +1,6 @@
 (ns hs-api.db
-  (:require [hs-api.queries :as query])
-  (:require [clojure.java.jdbc :as j]))
+  (:require [hs-api.queries :as query]
+            [clojure.java.jdbc :as j]))
 
 (def db {:dbtype "postgresql"
          :dbname "hs-db"
@@ -9,8 +9,9 @@
          :password (System/getenv "DB_PASSWORD")
          :stringtype "unspecified"})
 
+;; TODO
 ;; Init db
-(query/init-db db)
+(defn init [] (query/init-db db))
 
 ;; Create a signle patient
 (defn create-patient [patient]
@@ -39,10 +40,7 @@
                 :update-patient update-patient
                 :create-patient create-patient})
 
-
 (comment
-  (search-patients nil nil nil)
-  (search-patients nil "F" nil)
   (search-patients "098" "F" nil)
   (query/search-patients-sqlvec db {:search-query "TEST"})
   (create-patient {:name "Frank Cowperwood"
@@ -50,7 +48,6 @@
                    :address "Berlinerstrasse 124, Berlin, Germany"
                    :oms "1234567890123456"
                    :gender "M"})
-  (get-all-patients)
   (get-patient "b1393a03-8453-4f65-8b58-fd5631e66d66")
   (update-patient "86f6e38b-fa0a-40b8-bce5-903df0fb97d4"
                   {:id "b1393a03-8453-4f65-8b58-fd5631e66d66",
