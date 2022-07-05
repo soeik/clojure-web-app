326e6617d38b4fe4ce0b128358f2f5d6aa679c9c
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-07-05 12:58:21 +0200

    Add console output to the build file

--- build.clj
+++ build.clj
@@ -9,12 +9,16 @@
   (b/delete {:path "target"}))
 
 (defn uber [_]
+  (prinln "Start build")
   (clean nil)
+  (prinln "Copy files")
   (b/copy-dir {:src-dirs ["src/clj" "src/cljc" "src/cljs" "resources"]
                :target-dir class-dir})
+  (prinln "Compile clj")
   (b/compile-clj {:basis basis
                   :src-dirs ["src/clj" "src/cljc" "src/cljs"]
                   :class-dir class-dir})
+  (prinln "Uberjar")
   (b/uber {:class-dir class-dir
            :uber-file uber-file
            :basis basis
