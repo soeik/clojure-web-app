449117355b9c8cff29b686ae0fe3f6d1901e3439
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-07-07 23:54:00 +0200

    Run init-db on app start

--- src/clj/hs_api/server.clj
+++ src/clj/hs_api/server.clj
@@ -1,9 +1,11 @@
 (ns hs-api.server
   (:require
+   [ring.adapter.jetty :refer [run-jetty]]
    [hs-api.handler :refer [app]]
-   [ring.adapter.jetty :refer [run-jetty]])
+   [hs-api.db :refer [init-db]])
   (:gen-class))
 
 (defn -main [& args]
   (let [port 9000]
+    (init-db)
     (run-jetty #'app {:port port :join? false})))
