88a499f3294e3f7a0f09bebe90fa348039e2ff05
Kirill Stepanov <kirill.stepanov@cosuno.de>
2022-07-12 20:17:58 +0200

    Add re-frame

---
+++ src/cljs/app/core.cljs
@@ -0,0 +1,16 @@
+(ns app.core
+  (:require
+   [reagent.dom :as rdom]
+   [re-frame.core :as re-frame]
+   [app.events :as events]
+   [app.views :as views]))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (let [root-el (.getElementById js/document "app")]
+    (rdom/unmount-component-at-node root-el)
+    (rdom/render [views/main-panel] root-el)))
+
+(defn init []
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (mount-root))
